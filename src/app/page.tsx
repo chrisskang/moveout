ca8b6ea727f62d12c8b427e9cfb00025b159f95f
chrisskang <chris.kangaroo@icloud.com>
2025-01-07 18:55:00 +0100

    sold items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ interface SaleItem {
   price: number;
   paypalLink: string;
   images: string[];
+  sold: boolean;
 }
 
 const saleItems: SaleItem[] = [
@@ -21,35 +22,40 @@ const saleItems: SaleItem[] = [
     description: "Comfortable bean bag in mint condition, perfect for your living room. Original price €50.",
     price: 15,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/bean.jpg","/images/bean2.jpg"]
+    images: ["/images/bean.jpg","/images/bean2.jpg"],
+    sold: false
   },
   
   {    title: "Yoga Matt",
     description: "Orange yoga mat, 94cm x 200cm, barely used. Original price 40€.",
     price: 10,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/yomat.jpg",]
+    images: ["/images/yomat.jpg",],
+    sold: false
   },
 
   {    title: "Wardrobe",
     description: "Sturdy wardrobe with two compartments, perfect for storing your clothes and shoes. Size 80cm x 180cm x 50cm. IKEA",
     price: 0,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/wardrobe.jpg","/images/wardrobe2.jpg"]
+    images: ["/images/wardrobe.jpg","/images/wardrobe2.jpg"],
+    sold: false
   },
 
   {    title: "Chair",
     description: "Charming yellow chair, gently used, adds a vibrant touch to any room! Price for two",
     price: 10,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/chair.jpg",]
+    images: ["/images/chair.jpg",],
+    sold: true
   },
 
   {    title: "Dining Table",
     description: "White dining table for small kitchen. 80cm DIA. Original price 130€.",
     price: 50,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/kitchen_table.jpg",]
+    images: ["/images/kitchen_table.jpg",],
+    sold: false
   },
 
   {
@@ -57,25 +63,29 @@ const saleItems: SaleItem[] = [
     description: "IKEA POANG comfortable chair, good condition, extra cozy with a cushion. Original Price €129,99",
     price: 40,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/poang.jpg",]
+    images: ["/images/poang.jpg",],
+    sold: false
   },
   {    title: "Hanger",
     description: "Ikea MULIG hanger, gently used. 152 x 99 x 46 cm",
     price: 0,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/hanger.jpg",]
+    images: ["/images/hanger.jpg",],
+    sold: true
   },
   {    title: "Big Trash Can",
     description: "Pedal trash can, good condition and cleaned inside and out. 63 x 48 x 30cm & 50L. Original price 30€.",
     price: 10,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/big_trash.jpg",]
+    images: ["/images/big_trash.jpg",],
+    sold: false
   },
   {    title: "Medium Trash Can",
     description: "Trash can, good condition and cleaned inside and out. 47 x 34 x 26cm & 50L. Original price 21€.",
     price: 5,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/small_trash.jpg",]
+    images: ["/images/small_trash.jpg",],
+    sold: false
   },
 
   {
@@ -83,7 +93,8 @@ const saleItems: SaleItem[] = [
     description: "Good quality white table with shiny steel legs, 94.5 x 67cm. Expandible feature to 144.5cm (+26 both sides)",
     price: 10,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/white_table.jpg","/images/white_table2.jpg","/images/white_table3.jpg","/images/white_table4.jpg"]
+    images: ["/images/white_table.jpg","/images/white_table2.jpg","/images/white_table3.jpg","/images/white_table4.jpg"],
+    sold: false
   },
 
   {
@@ -91,28 +102,32 @@ const saleItems: SaleItem[] = [
     description: "Nice microwave + oven for various cooking needs, suitable for small house. Equipped with Air frying, steaming, roasting feature. Deep cleaned inside out. Original price €199 https://tinyurl.com/2ufx8znh",
     price: 100,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/microwave.jpg",]
+    images: ["/images/microwave.jpg",],
+    sold: false
   },
   {
     title: "Tall Shelf",
     description: "IKEA Shelf, 163cm x 33cm x 28cm, Painted myself, not so water friendly https://tinyurl.com/5xue5ryz",
     price: 0,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/tall_shelf.jpg","/images/tall_shelf2.jpg"]
+    images: ["/images/tall_shelf.jpg","/images/tall_shelf2.jpg"],
+    sold: false
   },
   {
     title: "Big Wardrobe",
     description: "IKEA Wardrobe, 176cm x 117cm x 36cm, two compartments, one with hanger, one with shelves. Original Price €119. Price negotiable https://tinyurl.com/e9jauavn",
     price: 30,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/big_wardrobe.jpg","/images/big_wardrobe2.jpg","/images/big_wardrobe3.jpg",]
+    images: ["/images/big_wardrobe.jpg","/images/big_wardrobe2.jpg","/images/big_wardrobe3.jpg",],
+    sold: false
   },
   {
     title: "Desk lamp",
     description: "IKEA KVART desk lamp",
     price: 10,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/lamp.jpg","/images/lamp2.jpg"]
+    images: ["/images/lamp.jpg","/images/lamp2.jpg"],
+    sold: false
   },
 
   {
@@ -120,42 +135,48 @@ const saleItems: SaleItem[] = [
     description: "Decent drawer 40 x 78cm, white, mint condition. Original Price €59,99 , https://tinyurl.com/49zr8pv2",
     price: 25,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/drawer.jpg","/images/drawer2.jpg",]
+    images: ["/images/drawer.jpg","/images/drawer2.jpg",],
+    sold: false
   },
   {
     title: "Shelf",
     description: "IKEA KALLAX 39 x 41.5 x 111.5cm, white, Original Price €44,99 , https://tinyurl.com/5a43uj8s",
     price: 15,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/shelf.jpg",]
+    images: ["/images/shelf.jpg",],
+    sold: false
   },
   {
     title: "Vacuum",
     description: "Clatronic BS 1300 N, 700W, compatible with filter SSB98/Swirl Y298 ",
     price: 0,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/vacuum.jpg","/images/vacuum2.jpg",]
+    images: ["/images/vacuum.jpg","/images/vacuum2.jpg",],
+    sold: false
   },
   {
     title: "Portfolio bag",
     description: "Good for carrying big size documents. A2 Size, water resistant - plastic material, cost for two",
     price: 10,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/portfolio_bag.jpg","/images/portfolio_bag2.jpg",]
+    images: ["/images/portfolio_bag.jpg","/images/portfolio_bag2.jpg",],
+    sold: false
   },
   {
     title: "Carpet",
     description: "Beige, 120 x 170cm, good condition",
     price: 0,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/carpet.jpg",]
+    images: ["/images/carpet.jpg",],
+    sold: false
   },
   {
     title: "Wood Table",
     description: "Gently used work table. Big enough for anything. Vertical adjustment possible. 120cm x 82cm",
     price: 10,
     paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/wood_table.jpg",]
+    images: ["/images/wood_table.jpg",],
+    sold: false
   },
 
 
@@ -205,7 +226,12 @@ export default function Home() {
     <>
       <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
         {saleItems.map((item) => (
-          <Card key={item.title} className="overflow-hidden">
+        <Card key={item.title} className="relative overflow-hidden">
+            {item.sold && (
+              <div className="absolute top-0 right-0 bg-red-500 text-white py-1 px-4 transform rotate-45 translate-x-[30%] translate-y-[30%] z-10">
+                Sold
+              </div>
+            )}
             <Dialog>
               <DialogTrigger asChild>
                 <div className="relative aspect-square cursor-pointer">
