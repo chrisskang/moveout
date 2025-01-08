59f845eb89b1fdb81fedf15773778f557c433f5b
chrisskang <chris.kangaroo@icloud.com>
2025-01-08 22:48:04 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,87 +15,40 @@ interface SaleItem {
   paypalLink: string;
   images: string[];
   sold: boolean;
+  reserved: boolean;
 }
 
 const saleItems: SaleItem[] = [
-  {    title: "Bean Bag",
-    description: "Comfortable bean bag in mint condition, perfect for your living room. Original price €50.",
-    price: 15,
-    paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/bean.jpg","/images/bean2.jpg"],
-    sold: false
-  },
   
-  {    title: "Yoga Matt",
-    description: "Orange yoga mat, 94cm x 200cm, barely used. Original price 40€.",
-    price: 10,
-    paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/yomat.jpg",],
-    sold: true
-  },
-
-  {    title: "Wardrobe",
-    description: "Sturdy wardrobe with two compartments, perfect for storing your clothes and shoes. Size 80cm x 180cm x 50cm. IKEA",
-    price: 0,
-    paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/wardrobe.jpg","/images/wardrobe2.jpg"],
-    sold: true
-  },
-
-  {    title: "Chair",
-    description: "Charming yellow chair, gently used, adds a vibrant touch to any room! Price for two",
-    price: 10,
-    paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/chair.jpg",],
-    sold: true
-  },
 
   {    title: "Dining Table",
     description: "White dining table for small kitchen. 80cm DIA. Original price 130€.",
     price: 50,
     paypalLink: "https://www.paypal.me/gyurikim968",
     images: ["/images/kitchen_table.jpg",],
-    sold: false
+    sold: false,
+    reserved: false
   },
 
-  {
-    title: "Poang Chair",
-    description: "IKEA POANG comfortable chair, good condition, extra cozy with a cushion. Original Price €129,99",
-    price: 40,
-    paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/poang.jpg",],
-    sold: true
-  },
-  {    title: "Hanger",
-    description: "Ikea MULIG hanger, gently used. 152 x 99 x 46 cm",
-    price: 0,
-    paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/hanger.jpg",],
-    sold: true
-  },
+  
   {    title: "Big Trash Can",
     description: "Pedal trash can, good condition and cleaned inside and out. 63 x 48 x 30cm & 50L. Original price 30€.",
     price: 10,
     paypalLink: "https://www.paypal.me/gyurikim968",
     images: ["/images/big_trash.jpg",],
-    sold: false
+    sold: false,
+    reserved: false
   },
   {    title: "Medium Trash Can",
     description: "Trash can, good condition and cleaned inside and out. 47 x 34 x 26cm & 50L. Original price 21€.",
     price: 5,
     paypalLink: "https://www.paypal.me/gyurikim968",
     images: ["/images/small_trash.jpg",],
-    sold: false
+    sold: false,
+    reserved: false
   },
 
-  {
-    title: "White Table",
-    description: "Good quality white table with shiny steel legs, 94.5 x 67cm. Expandible feature to 144.5cm (+26 both sides)",
-    price: 10,
-    paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/white_table.jpg","/images/white_table2.jpg","/images/white_table3.jpg","/images/white_table4.jpg"],
-    sold: true
-  },
+  
 
   {
     title: "Microwave Oven",
@@ -103,31 +56,18 @@ const saleItems: SaleItem[] = [
     price: 100,
     paypalLink: "https://www.paypal.me/gyurikim968",
     images: ["/images/microwave.jpg",],
-    sold: false
+    sold: false,
+    reserved: false
   },
-  {
-    title: "Tall Shelf",
-    description: "IKEA Shelf, 163cm x 33cm x 28cm, Painted myself, not so water friendly https://tinyurl.com/5xue5ryz",
-    price: 0,
-    paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/tall_shelf.jpg","/images/tall_shelf2.jpg"],
-    sold: true
-  },
-  {
-    title: "Big Wardrobe",
-    description: "IKEA Wardrobe, 176cm x 117cm x 36cm, two compartments, one with hanger, one with shelves. Original Price €119. Price negotiable https://tinyurl.com/e9jauavn",
-    price: 30,
-    paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/big_wardrobe.jpg","/images/big_wardrobe2.jpg","/images/big_wardrobe3.jpg",],
-    sold: true
-  },
+  
   {
     title: "Desk lamp",
     description: "IKEA KVART desk lamp",
     price: 10,
     paypalLink: "https://www.paypal.me/gyurikim968",
     images: ["/images/lamp.jpg","/images/lamp2.jpg"],
-    sold: false
+    sold: false,
+    reserved: false
   },
 
   {
@@ -136,7 +76,8 @@ const saleItems: SaleItem[] = [
     price: 25,
     paypalLink: "https://www.paypal.me/gyurikim968",
     images: ["/images/drawer.jpg","/images/drawer2.jpg",],
-    sold: false
+    sold: false,
+    reserved: false
   },
   {
     title: "Shelf",
@@ -144,7 +85,113 @@ const saleItems: SaleItem[] = [
     price: 15,
     paypalLink: "https://www.paypal.me/gyurikim968",
     images: ["/images/shelf.jpg",],
-    sold: false
+    sold: false,
+    reserved: false
+  },
+  
+  {
+    title: "Portfolio bag",
+    description: "Good for carrying big size documents. A2 Size, water resistant - plastic material, cost for two",
+    price: 10,
+    paypalLink: "https://www.paypal.me/gyurikim968",
+    images: ["/images/portfolio_bag.jpg","/images/portfolio_bag2.jpg",],
+    sold: false,
+    reserved: false
+  },
+
+  {    title: "Hanger",
+    description: "Ikea MULIG hanger, gently used. 152 x 99 x 46 cm",
+    price: 0,
+    paypalLink: "https://www.paypal.me/gyurikim968",
+    images: ["/images/hanger.jpg",],
+    sold: false,
+    reserved: true
+    
+  },
+
+  {    title: "Yoga Matt",
+    description: "Orange yoga mat, 94cm x 200cm, barely used. Original price 40€.",
+    price: 10,
+    paypalLink: "https://www.paypal.me/gyurikim968",
+    images: ["/images/yomat.jpg",],
+    sold: false,
+    reserved: true
+    
+  },
+  {    title: "Chair",
+    description: "Charming yellow chair, gently used, adds a vibrant touch to any room! Price for two",
+    price: 10,
+    paypalLink: "https://www.paypal.me/gyurikim968",
+    images: ["/images/chair.jpg",],
+    sold: false,
+    reserved: true
+    
+  },
+
+  
+
+  {    title: "Bean Bag",
+    description: "Comfortable bean bag in mint condition, perfect for your living room. Original price €50.",
+    price: 15,
+    paypalLink: "https://www.paypal.me/gyurikim968",
+    images: ["/images/bean.jpg","/images/bean2.jpg"],
+    sold: true,
+    reserved: true
+  },
+  
+
+  {    title: "Wardrobe",
+    description: "Sturdy wardrobe with two compartments, perfect for storing your clothes and shoes. Size 80cm x 180cm x 50cm. IKEA",
+    price: 0,
+    paypalLink: "https://www.paypal.me/gyurikim968",
+    images: ["/images/wardrobe.jpg","/images/wardrobe2.jpg"],
+    sold: true,
+    reserved: true
+    
+  },
+
+
+
+  {
+    title: "Poang Chair",
+    description: "IKEA POANG comfortable chair, good condition, extra cozy with a cushion. Original Price €129,99",
+    price: 40,
+    paypalLink: "https://www.paypal.me/gyurikim968",
+    images: ["/images/poang.jpg",],
+    sold: true,
+    reserved: true
+    
+  },
+  
+  {
+    title: "White Table",
+    description: "Good quality white table with shiny steel legs, 94.5 x 67cm. Expandible feature to 144.5cm (+26 both sides)",
+    price: 10,
+    paypalLink: "https://www.paypal.me/gyurikim968",
+    images: ["/images/white_table.jpg","/images/white_table2.jpg","/images/white_table3.jpg","/images/white_table4.jpg"],
+    sold: true,
+    reserved: true
+    
+  },
+  {
+    title: "Tall Shelf",
+    description: "IKEA Shelf, 163cm x 33cm x 28cm, Painted myself, not so water friendly https://tinyurl.com/5xue5ryz",
+    price: 0,
+    paypalLink: "https://www.paypal.me/gyurikim968",
+    images: ["/images/tall_shelf.jpg","/images/tall_shelf2.jpg"],
+    sold: true,
+    reserved: true
+    
+  },
+  {
+    title: "Big Wardrobe",
+    description: "IKEA Wardrobe, 176cm x 117cm x 36cm, two compartments, one with hanger, one with shelves. Original Price €119. Price negotiable https://tinyurl.com/e9jauavn",
+    price: 30,
+    paypalLink: "https://www.paypal.me/gyurikim968",
+    images: ["/images/big_wardrobe.jpg","/images/big_wardrobe2.jpg","/images/big_wardrobe3.jpg",],
+    sold: true,
+    reserved: true
+    
   },
   {
     title: "Vacuum",
@@ -152,23 +199,20 @@ const saleItems: SaleItem[] = [
     price: 0,
     paypalLink: "https://www.paypal.me/gyurikim968",
     images: ["/images/vacuum.jpg","/images/vacuum2.jpg",],
-    sold: true
+    sold: true,
+    reserved: true
+    
   },
-  {
-    title: "Portfolio bag",
-    description: "Good for carrying big size documents. A2 Size, water resistant - plastic material, cost for two",
-    price: 10,
-    paypalLink: "https://www.paypal.me/gyurikim968",
-    images: ["/images/portfolio_bag.jpg","/images/portfolio_bag2.jpg",],
-    sold: false
-  },
+
   {
     title: "Carpet",
     description: "Beige, 120 x 170cm, good condition",
     price: 0,
     paypalLink: "https://www.paypal.me/gyurikim968",
     images: ["/images/carpet.jpg",],
-    sold: true
+    sold: true,
+    reserved: true
+    
   },
   {
     title: "Wood Table",
@@ -176,9 +220,10 @@ const saleItems: SaleItem[] = [
     price: 10,
     paypalLink: "https://www.paypal.me/gyurikim968",
     images: ["/images/wood_table.jpg",],
-    sold: true
+    sold: true,
+    reserved: true
+    
   },
-
 
 
 
@@ -227,11 +272,18 @@ export default function Home() {
       <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
         {saleItems.map((item) => (
         <Card key={item.title} className="relative overflow-hidden">
-            {item.sold && (
-              <div className="absolute top-5 right-5 bg-orange-500 text-2xl text-white py-1 px-10 transform rotate-45 translate-x-[30%] translate-y-[30%] z-10">
+            {item.reserved ?
+            item.sold ?
+              (<div className="absolute top-5 right-5 bg-red-500 text-2xl text-center  text-white py-1 px-20 transform rotate-45 translate-x-[30%] translate-y-[30%] z-10">
+                Sold
+              </div>)
+
+              : (<div className="absolute top-8 right-5 bg-orange-500 text-2xl text-center text-white py-1 px-20 transform rotate-45 translate-x-[30%] translate-y-[30%] z-10">
                 Reserved
-              </div>
-            )}
+              </div>)
+            : null
+            
+            }
             <Dialog>
               <DialogTrigger asChild>
                 <div className="relative aspect-square cursor-pointer">
